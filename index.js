7480bd5e24113d076d27a2e11df5c5b1d34e65f9
dingming <ddd>
2023-07-07 18:28:50 +0800

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+/* 引入express框架 */
+const express = require('express');
+const api = require('./api')
+const connStr = require('./dbConfig')
+
+var router = express.Router();
+const app = express();
+/* 引入cors */
+const cors = require('cors');
+app.use(cors());
+/* 引入body-parser */
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+/* 引入mysql */
+const mysql = require('mysql');
+
+
+
+
+
+function handleDisconnection(){
+    const conn = mysql.createConnection(connStr)
+    conn.connect( err=>{
+        if (err) {
+            setTimeout('handleDisconnection()', 2000);
+        }
+    });
+
+    conn.on('error', function (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnection();
+        } else {
+            throw err;
+        }
+    });
+
+    api(app,conn)
+}
+
+handleDisconnection()
+
+
+
+
+
+
---
+++ 原服务端/server.js
@@ -0,0 +1,125 @@
+/* 引入express框架 */
+const express = require('express');
+var router = express.Router();
+const app = express();
+/* 引入cors */
+const cors = require('cors');
+app.use(cors());
+/* 引入body-parser */
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+/* 引入mysql */
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'test',
+    multipleStatements: true
+})
+conn.connect();
+/* 监听端口 */
+app.listen(3000, () => {
+    console.log('——————————服务已启动——————————');
+})
+app.get('/', (req, res) => {
+    res.send('<p style="color:red">服务已启动</p>');
+})
+//查
+app.get('/api/getUserList', (req, res) => {
+    const sqlStr = 'SELECT * FROM users'
+    conn.query(sqlStr, (error, results) => {
+        if (error) return res.json({
+            code: 404,
+            message: error
+        })
+        res.json({
+            code: 200,
+            message: results,
+            affextedRows: results.affextedRows
+        })
+    })
+})
+//条件查找
+app.get('/api/getUserListById/:id', (req, res) => {
+    const id = req.params.id;
+    const sqlStr = 'select * from users where id=?';
+    conn.query(sqlStr, id, (err, results) => {
+        if (err) return res.json({
+            code: 404,
+            message: '数据不存在',
+            affextedRows: 0
+        });
+        res.json({
+            code: 200,
+            message: results,
+            affextedRows: results.affextedRows
+        });
+    })
+});
+//添加
+app.post('/api/addUser', (req, res) => {
+    console.log(req.body);
+    const username = req.body.username;
+    const sex = req.body.sex;
+    const address = req.body.address;
+    console.log("username:" + username);
+    // const sqlStr = "insert into users(username,sex,address) values('" + username + "','" + sex + "','" + address + "')";
+    const sqlStr = "insert into users set ?";
+    console.log(sqlStr);
+    conn.query(sqlStr, req.body, (err, results) => {
+        if (err) return res.json({
+            code: 404,
+            message: err,
+            affectedRows: 0
+        });
+        res.json({
+            code: 200,
+            message: '添加成功',
+            affectedRows: results.affectedRows
+        });
+    })
+});
+//修改
+app.post('/api/updateUser', function (req, res) {
+    console.log(req.body);
+    const id = req.body.id;
+    const username = req.body.username;
+    const sex = req.body.sex;
+    const address = req.body.address;
+    const sqlStr = "update users set username='" + username + "',sex='" + sex + "',address='" + address + "'where id=" + id;
+    console.log(sqlStr);
+    conn.query(sqlStr, (err, results) => {
+        if (err) return res.json({
+            code: 404,
+            message: err,
+            affectedRows: 0
+        });
+        res.json({
+            code: 200,
+            message: '修改成功',
+            affectedRows: results.affectedRows
+        });
+    })
+});
+//删除
+app.delete('/api/deleteUser/:id', function (req, res) {
+    console.log('params:' + req.params);
+    let id = req.params.id;
+    let sql = 'delete from users where id=' + id;
+    conn.query(sql, id, (err, results) => {
+        if (err) return res.json({
+            code: 404,
+            message: err,
+            affectedRows: 0
+        });
+        res.json({
+            code: 200,
+            message: '删除成功',
+            affectedRows: results.affectedRows
+        })
+    })
+});
